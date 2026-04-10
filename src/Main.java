67834ec6082266c393f91c06376599fe96fa390e
julianandresvazquezzgarcia-arch <julianandresvazquezzgarcia@gmail.com>
2026-04-09 21:16:23 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,24 @@
 public class Main {
     public static void main(String[] args) {
 
-        Vehiculo v1 = VehiculoFactory.crearVehiculo("auto");
-        v1.placa = "ABC123";
+      
+        Vehiculo[] flota = new Vehiculo[3];
 
-        Vehiculo v2 = VehiculoFactory.crearVehiculo("van");
-        v2.placa = "XYZ789";
+        flota[0] = VehiculoFactory.crearVehiculo("auto");
+        flota[0].placa = "ABC123";
 
-        v1.mostrarInfo();
-        v2.mostrarInfo();
+        flota[1] = VehiculoFactory.crearVehiculo("van");
+        flota[1].placa = "XYZ789";
 
+        flota[2] = VehiculoFactory.crearVehiculo("camion");
+        flota[2].placa = "LMN456";
+
+   
+        for (int i = 0; i < flota.length; i++) {
+            flota[i].mostrarInfo();
+        }
+
+        
         Contrato c = new ContratoBuilder()
                 .setCliente("Juan")
                 .setVehiculo("Auto")
