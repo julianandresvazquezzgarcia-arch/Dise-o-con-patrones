4a61ff1dc9f27a9fc84dcdb209c03083938b459e
julianandresvazquezzgarcia-arch <julianandresvazquezzgarcia@gmail.com>
2026-04-23 18:20:41 -0500

    Delete src/Camion.java

--- src/Camion.java
+++
@@ -1,9 +0,0 @@
-class Camion extends Vehiculo {
-    public Camion() {
-        this.autonomia = 600;
-    }
-
-    public void mostrarInfo() {
-        System.out.println("Camion: " + placa);
-    }
-}
